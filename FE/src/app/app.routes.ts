60a1affe5c7dd7bd687d74557220720e5dd008a3
Jovan Brzakovic <jova3105@gmail.com>
2024-07-22 10:24:33 +0200

    Added guards, dashboard init

--- FE/src/app/app.routes.ts
+++ FE/src/app/app.routes.ts
@@ -1,8 +1,29 @@
 import { Routes } from '@angular/router';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { isNotLoggedInGuard } from './shared/guards/is-not-logged-in.guard';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { isLoggedInGuard } from './shared/guards/is-logged-in.guard';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginPageComponent },
-  { path: 'register', component: RegisterPageComponent },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+    canActivate: [isNotLoggedInGuard],
+  },
+  {
+    path: 'register',
+    component: RegisterPageComponent,
+    canActivate: [isNotLoggedInGuard],
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [isLoggedInGuard],
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard',
+  },
 ];
