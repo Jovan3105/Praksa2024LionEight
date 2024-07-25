f0ccaf83ea80e99deb4253648528c91497ba0ab9
Djordje Ljubisavljevic <djordjeljubisavljevic74x@gmail.com>
2024-07-25 03:51:30 -0700

    Index and NativeQuery

--- BE/src/main/java/com/example/demo/dao/UserRepository.java
+++ BE/src/main/java/com/example/demo/dao/UserRepository.java
@@ -2,10 +2,12 @@ package com.example.demo.dao;
 
 import com.example.demo.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Users,Integer> {
+    @Query(value = "SELECT * FROM users WHERE email = ?1", nativeQuery = true)
     Optional<Users> findByEmail(String email);
 }
